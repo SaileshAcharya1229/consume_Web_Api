47b4e02f5636d0a9c287ce72f454ef21d8d08b7d
unknown <saileshacharya1229@gmai.com>
2023-05-18 18:06:20 +0545

    consume api

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using ConsumeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private string baseURL = "https://localhost:7181";
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //calling the web api and populating the data in  view  using dataTable
+
+            DataTable dt = new DataTable();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("Contacts");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    dt = JsonConvert.DeserializeObject<DataTable>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error Calling web API");
+
+                }
+                ViewData.Model = dt;
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Index2()
+        {
+            //calling the web api and populating the data in  view  using model class
+
+           IList<UserEntity> user = new List<UserEntity>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("Contacts");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    user = JsonConvert.DeserializeObject<List<UserEntity>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error Calling web API");
+
+                }
+                ViewData.Model = user;
+            }
+
+            return View();
+        }
+
+        public async Task<ActionResult<string>> AddUser(UserEntity user)
+        {
+            UserEntity obj = new UserEntity()
+            {
+                FullName = user.FullName,
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address
+            };
+
+            if(user.FullName != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseURL);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage getData = await client.PostAsJsonAsync("Contacts", obj);
+
+                    if (getData.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error Calling web API");
+
+                    }
+                }
+
+
+                }
+                return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
